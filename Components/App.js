c8f5bd083c41a6881568668906b7e51185d5b7d1
Michael <mschneider247@gmail.com>
2020-05-12 13:27:42 -0600

    Add component files fix package.json for local deployment

---
+++ Components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [],
+    }
+  }
+
+  render () {
+    return (
+      <>
+        <h1>Todo it App!</h1>
+      </>
+    )
+  }
+}
+
+export default App;
